951f2afa137f8d0a2ffaa345ac99f6cd20d5f8b1
Hadi Saleh <hadi.saleh852@outlook.com>
2019-05-13 12:51:42 +0200

    initial

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # n-dimensional-random
-An n-dimensional random number generator
+An n-dimensional random number generator.
+
+
+Example:
+
+```javascript
+const RNG = require('rng.js');
+
+const next = RNG(Date.now(), 2);
+
+let rand1 = next(0, 0), rand2 = next(1, 14);
+```
---
+++ rng.js
@@ -0,0 +1,16 @@
+const repeat = (n, e) => Array(n).fill(e);
+const range = n => Array(n).fill(0).map((_, i) => i);
+
+const fract = f => f % 1;
+
+const dot = (arr1, arr2) => arr1.reduce((a, c, i) => a + c * arr2[i], 0);
+
+const RNG = function (seed, dimension) {
+    const vec = range(dimension).map(x =>
+        fract(Math.sin((seed + x + .5) * 12.9898) * 43758.5453123)
+    );
+
+    return (...point) => fract(Math.abs(Math.sin(dot(point, vec))) * 43758.5453123);
+};
+
+exports = RNG;
